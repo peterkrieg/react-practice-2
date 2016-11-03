1c57a4e6db5ae68ff0b623365800547b16ccba61
Peter Krieg <pkrieg2@gmail.com>
2016-11-02 21:41:09 -0400

    people-table data repeating

--- src/PeopleTable.js
+++ src/PeopleTable.js
@@ -2,24 +2,34 @@ import React, { Component } from 'react';
 import Person from './Person';
 
 class PeopleTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: [],
+    }
+  }
   componentDidMount() {
     console.log('component did mount');
+
+    fetch('http://swapi.co/api/people/')
+    .then(response => response.json())
+    .then(people => {
+      people = people.results;
+      this.setState({ people });
+    });
   }
   renderPeople() {
-    return (
-      <div>
-        <Person name='luke' />
-        <Person name={'leia'} />
-        <Person name={'han'} />
-      </div>
-    );
+    console.log(this.state);
+    return;
+    return this.state.people.map(person => <li>{person.name}</li>);
   }
   render() {
     console.log('render');
     return (
-      <div>
-        { this.renderPeople() }
-      </div>
+      <ul>
+        {this.renderPeople()}
+      </ul>
     );
   }
 }
