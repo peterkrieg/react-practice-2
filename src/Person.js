2d955b67d7e1800bfa18f78c55128669f9be55f3
Peter Krieg <pkrieg2@gmail.com>
2016-11-02 21:55:10 -0400

    added styles

--- src/Person.js
+++ src/Person.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+const list = {
+	listStyleType: 'none',
+	width: 100,
+	height: 200,
+	border: '1px solid black',
+	float: 'left',
+	marginRight: 50,
+}
+
 export default class Person extends Component {
 	render() {
+		const { name, height, gender, eye_color } = this.props.person;
 		return (
-			<li>{this.props.person.name}</li>
+			<li style={list}>
+				<h4>{name}</h4>
+				<p>height: {height}</p>
+			</li>
 		);
 	}
 }
