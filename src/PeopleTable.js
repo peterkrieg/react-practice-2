809655d78f17fa85e9ecc0aca466f451530e59fd
Peter Krieg <pkrieg2@gmail.com>
2016-11-02 21:19:21 -0400

    simple method

--- src/PeopleTable.js
+++ src/PeopleTable.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Person from './Person';
 
 class PeopleTable extends Component {
-  render() {
+  renderPeople() {
     return (
       <div>
         <Person name='luke' />
         <Person name={'leia'} />
         <Person name={'han'} />
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div>
+        { this.renderPeople() }
       </div>
     );
   }
